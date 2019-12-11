a09d71ddd144bbc3157edcb4e4f00663e9c79330
Danny Joe Dozan <joe.dozan24@gmail.com>
2019-12-11 18:04:11 +0700

    Model v1.1 - Strings & Columns

--- models/mahasiswa.js
+++ models/mahasiswa.js
@@ -4,18 +4,19 @@ module.exports = {
     },
     
     getById: function(conn, id, callback) {
-        conn.query("SELECT * FROM mahasiswa WHERE NIM = ${id}", callback)
+        conn.query("SELECT * FROM mahasiswa WHERE NIM = '${id}'", callback)
     },
     
     create: function(conn, data, callback) {
-        conn.query("INSERT INTO mahasiswa SET NIM = ${data.nim}, nama = ${data.nama}, tgl_lahir = ${data.tgl_lahir}, tahun_masuk = ${data.tahun_masuk}, kode_program_studi = ${data.kode_program_studi}", callback)
+        console.log(data);
+        conn.query("INSERT INTO mahasiswa SET NIM = '${data.nim}', nama = '${data.nama}', tempat_lahir = '${data.tempat_lahir}', tgl_lahir = '${data.tgl_lahir}', tahun_masuk = '${data.tahun_masuk}', kode_program_studi = '${data.kode_program_studi}'", callback)
     },
     
     update: function(conn, data, id, callback) {
-        conn.query("UPDATE mahasiswa SET NIM = ${data.nim}, nama = ${data.nama}, tgl_lahir = ${data.tgl_lahir}, tahun_masuk = ${data.tahun_masuk}, kode_program_studi = ${data.kode_program_studi} WHERE NIM = ${id}", callback)
+        conn.query("UPDATE mahasiswa SET NIM = '${data.nim}', nama = '${data.nama}', tempat_lahir = '${data.tempat_lahir}', tgl_lahir = '${data.tgl_lahir}', tahun_masuk = '${data.tahun_masuk}', kode_program_studi = '${data.kode_program_studi}' WHERE NIM = '${id}'", callback)
     },
     
     delete: function(conn, id, callback) {
-        conn.query("DELETE FROM mahasiswa WHERE NIM = ${id}", callback)
+        conn.query("DELETE FROM mahasiswa WHERE NIM = '${id}'", callback)
     }
 }
